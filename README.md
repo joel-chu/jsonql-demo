1e0d96827034ee57ef873094f043b895f878a8c2
joel-chu <newbranltd@gmail.com>
2019-08-28 15:07:28 +0100

    all the demo done with README updated

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # jsonql-demo
-jsonql demo using our Koa middleware to show a complete server (node.js) and client (browser) setup 
+jsonql demo using our Koa middleware to show a complete server (node.js) and client (browser) setup
+
+## setup
+
+First clone the repo to your local:
+
+```sh
+$ git clone https://github.com/joel-chu/jsonql-demo
+```
+Then install dependencies
+```sh
+$ cd jsonql-demo && npm install
+```
+
+And finally just run the start command:
+
+```sh
+$ npm start
+```
+
+Your browser will open automatically, you can watch the console debugger. Also your browser development console.
+
+---
+
+Joel Chu (c) 2019
--- public/app.js
+++ public/app.js
@@ -1,5 +1,51 @@
 // the core code to run the demo
 (function() {
-  console.log('hello world!')
+  var error = 'The client is NOT ready!'
+  var globalClient; // for caching
+  jsonqlClient()
+    .then(function(client) {
+      globalClient = client;
+      client.query.helloWorld()
+        .then(function(msg) {
+          var s= document.getElementById('hello')
+          setTimeout(function() {
+            s.innerHTML = msg
+            s.className = 'has-text-success'
+          }, 2000)
+        })
+    })
+
+  window.getSomething = function() {
+    if (globalClient) {
+      globalClient.query.getSomething()
+        .then(function(result) {
+          var r1 = document.getElementById('result1')
+          r1.innerHTML = result;
+          r1.className = 'has-text-link'
+        })
+    } else {
+      alert(error)
+    }
+  }
+
+  window.saveSomething = function() {
+    if (globalClient) {
+      var payload = document.getElementById('payload').value
+      var condition = parseInt(document.getElementById('condition').value)
+
+      globalClient.mutation.saveSomething(payload, condition)
+        .then(function(result) {
+          var r2 = document.getElementById('result2')
+          r2.innerHTML = result
+          r2.className = 'has-text-link'
+        })
+        .catch(function(err) {
+          console.error(err)
+        })
+    } else {
+      alert(error)
+    }
+  }
+
 
 })()
